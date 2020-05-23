bbcb3f662193cd46d1b2e34d8beddd95fbb78238
Anju <anjurose.d@gmail.com>
2020-05-23 05:37:20 +0000

    Added task 7

---
+++ 7_modules/init.pp
@@ -0,0 +1,14 @@
+exec { 'apt-update':                    # exec resource named 'apt-update'
+  command => '/usr/bin/apt-get update'  # command this resource will run
+}
+
+# install tree package
+package { 'nginx':
+  require => Exec['apt-update'],       # require 'apt-update' before installing
+  ensure => installed,
+}
+service { 'nginx':
+  require => Exec['apt-update'],       # require 'apt-update' before installing
+  ensure => started,
+}
+
